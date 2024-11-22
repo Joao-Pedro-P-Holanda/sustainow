99b10e5bc281044c6fad2923fb38dc959062ee88
PHS <moreira.phs@alu.ufc.br>
2024-11-22 19:57:15 -0300

    SplashScreen implemented

--- app/src/main/java/io/github/sustainow/MainActivity.kt
+++ app/src/main/java/io/github/sustainow/MainActivity.kt
@@ -12,11 +12,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.github.sustainow.presentation.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             AppTheme{
