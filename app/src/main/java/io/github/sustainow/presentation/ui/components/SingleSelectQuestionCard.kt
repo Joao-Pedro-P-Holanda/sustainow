b0d7d08c617104c9c6e7c5185e1cea662c9a19c2
Francisco Paulino Arruda Filho <fpaulinofilho04@gmail.com>
2024-12-01 11:31:09 -0300

    chore: remove impots* and adding individual imports

--- app/src/main/java/io/github/sustainow/presentation/ui/components/SingleSelectQuestionCard.kt
+++ app/src/main/java/io/github/sustainow/presentation/ui/components/SingleSelectQuestionCard.kt
@@ -1,20 +1,34 @@
 package io.github.sustainow.presentation.ui.components
 
 import android.util.Log
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.github.sustainow.domain.model.Question
 import io.github.sustainow.domain.model.QuestionAlternative
-import io.github.sustainow.presentation.theme.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.sustainow.presentation.theme.AppTheme
+import io.github.sustainow.presentation.theme.onSurfaceDarkHighContrast
+import io.github.sustainow.presentation.theme.scrimLight
 import kotlin.time.Duration
 
 @Composable
